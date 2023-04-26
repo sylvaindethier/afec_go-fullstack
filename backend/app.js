c69d39de54b54c187e592dd5d07959024a9b3514
sylvaindethier <dethiersylvain@gmail.com>
2023-04-26 19:24:03 +0200

    Course 09

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const stuffRoutes = require("./routes/stuff");
+
+const Thing = require("./models/Thing");
+
+const app = express();
+
+// connect to MongoDB Altas
+const MONGODB_PASSWORD = "KSWsK8A6SmzhnYeD";
+const MONGODB_SRV = `mongodb+srv://dethiersylvain:${MONGODB_PASSWORD}@clusterafec.rtrzz0l.mongodb.net/test`;
+mongoose
+  .connect(MONGODB_SRV, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+// JSON middleware
+app.use(express.json());
+
+// Headers middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+
+  next();
+});
+
+
+// Use stuff Routes
+app.use("/api/stuff", stuffRoutes);
+
+module.exports = app;
---
+++ backend/routes/stuff.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const stuffController = require("../controllers/stuff");
+const Thing = require("../models/Thing");
+
+// Create Router
+const router = express.Router();
+
+// POST: Create a Thing
+router.post("/", stuffController.createThing);
+
+// GET: Get a Thing by id
+router.get("/:id", stuffController.getOneThing);
+
+// PUT: Update a Thing by id
+router.put("/:id", stuffController.updateThing);
+
+// DELETE: Delete a Thing by id
+router.delete("/:id", stuffController.deleteThing);
+
+// GET: Get all Thing
+router.get("/", stuffController.getAllThing);
+
+module.exports = router;
